46e21bab453d8f349f5e5cbf37847bce52afff21
GMNSA <kohotokk@mail.ru>
2022-04-09 09:16:06 +0300

    develop | test f_string

--- libs/tests/main.c
+++ libs/tests/main.c
@@ -1,7 +1,45 @@
 #include <stdio.h>
+#include "f_string.h"
+
+void test_strlen();
 
 int main(void)
 {
-	puts("TEST OK");
+#ifdef _TEST_1
+	test_strlen();
+#endif
 	return 0;
+}
+
+void test_strlen() {
+	if (f_strlen("  ") != 2) {
+		printf("n/a\n");
+	} else {
+		printf("OK\n");
+	}
+
+	if (f_strlen("There") == 5) {
+		printf("OK\n");
+	} else {
+		printf("n/a\n");
+	}
+
+	if (f_strlen("There is no spoon.") == 18) {
+		printf("OK\n");
+	} else {
+		printf("n/a\n");
+	}
+
+	if (f_strlen("T") == 1) {
+		printf("OK\n");
+	} else {
+		printf("n/a\n");
+	}
+
+	if (f_strlen("") == 0) {
+		printf("OK\n");
+	} else {
+		printf("n/a\n");
+	}
+	printf("TEST strlen OK\n");
 }
